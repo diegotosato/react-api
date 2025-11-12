db74385d88fad8c8b27e0424b9e3bdfabe310098
diegotosato <tosato1998@gmail.com>
2025-11-12 15:00:42 +0100

    install and import axios, declare a function to start an ajax call to store into a variable its content, print in console the content of the endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 function App() {
+
+  const attriciEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
+
+  const [attrici, setAttrici] = useState([])
+
+  function handleAttrici() {
+    axios.get(attriciEndpoint)
+      .then(res => setAttrici(res.data))
+  }
+
+  useEffect(handleAttrici, [])
+  console.log(attrici);
+
+
+
 
   return (
     <>
