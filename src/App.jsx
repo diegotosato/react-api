d4cd566a859e155c8fd9dc0182e3997078175b4e
diegotosato <tosato1998@gmail.com>
2025-11-12 15:41:37 +0100

    add comments to the code, change italian words into english words, change some css rules

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 function App() {
+  //AjaxCall#1 endpoint into a variable
+  const actressesEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
 
-  const attriciEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
+  //variable to collect ajax call content with initial state of empty array
+  const [actresses, setAtresses] = useState([])
 
-  const [attrici, setAttrici] = useState([])
-
-  function handleAttrici() {
-    axios.get(attriciEndpoint)
-      .then(res => setAttrici(res.data))
+  //function to do the ajax call
+  function handleAtresses() {
+    axios.get(actressesEndpoint)
+      .then(res => setAtresses(res.data))
   }
-
-  useEffect(handleAttrici, [])
-  console.log(attrici);
+  //manage the function to be done only one time at the load of the component
+  useEffect(handleAtresses, [])
+  console.log(actresses);
 
 
 
@@ -22,18 +24,19 @@ function App() {
     <>
       <h1>API</h1>
 
+      {/* map the array to create a card for every object (actresses) */}
       {
-        attrici.map(attrice => (
-          <div key={attrice.id} className="card">
+        actresses.map(actress => (
+          <div key={actress.id} className="card">
             <div className="card_image">
-              <img src={attrice.image} alt={attrice.name} />
+              <img src={actress.image} alt={actress.name} />
             </div>
-            <div className="dati_attrice">
-              <h3>{attrice.name}</h3>
-              <p>{attrice.birth_year}</p>
-              <p>{attrice.nationality}</p>
-              <p>{attrice.biography}</p>
-              <p>{attrice.awards}</p>
+            <div className="dati_actress">
+              <h3>{actress.name}</h3>
+              <p>{actress.birth_year}</p>
+              <p>{actress.nationality}</p>
+              <p>{actress.biography}</p>
+              <p>{actress.awards}</p>
             </div>
           </div>
         ))
