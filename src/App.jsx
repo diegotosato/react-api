481cd871247b10253df005b493080036523bf2ca
diegotosato <tosato1998@gmail.com>
2025-11-12 17:02:01 +0100

    create empty array and fill with the two precedent arrays, substitute the array to map with the new one

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   //variable to collect ajax call content with initial state of empty array
   const [actresses, setActresses] = useState([])
 
-  //function to do the ajax call
+  // function to do the ajax call
   function handleActresses() {
     axios.get(actressesEndpoint)
       .then(res => setActresses(res.data))
@@ -21,13 +21,13 @@ function App() {
 
 
 
-  //AjaxCall#1 endpoint into a variable
+  //AjaxCall#2 endpoint into a variable
   const actorsEndpoint = 'https://lanciweb.github.io/demo/api/actors/'
 
   //variable to collect ajax call content with initial state of empty array
   const [actors, setActors] = useState([])
 
-  //function to do the ajax call
+  // function to do the ajax call
   function handleActors() {
     axios.get(actorsEndpoint)
       .then(res => setActors(res.data))
@@ -38,51 +38,32 @@ function App() {
 
 
 
+
+  //emprty array to collect the two precedent arrays
+  const allActors = []
+
+  //function to puh the two arrays into the empty third
+  allActors.push(...actresses, ...actors)
+
+  console.log(allActors);
+
+
+
+
+
   return (
     <>
       {/* Actress */}
-      <h1>Actress</h1>
+      <h1>All Actors</h1>
 
       <div className="container">
 
         <div className="row">
 
-          {/* map the array to create a card for every object (actresses) */}
+          {/* map the array that contain both two precedent */}
           {
-            actresses.map(actress => (
-              <div key={actress.id} className="card">
-                <div className="card_image">
-                  <img src={actress.image} alt={actress.name} />
-                </div>
-                <div className="card_details">
-                  <h2 className="card_details_name">{actress.name}</h2>
-                  <p className="card_details_birth">{actress.birth_year}</p>
-                  <p className="card_details_nation">{actress.nationality}</p>
-                  <p className="card_details_bio">{actress.biography}</p>
-                  <p className="card_details_awards">{actress.awards}</p>
-                </div>
-              </div>
-            ))
-          }
-
-        </div>
-
-      </div>
-
-
-
-
-      {/* Actors */}
-      <h1>Actors</h1>
-
-      <div className="container">
-
-        <div className="row">
-
-          {/* map the array to create a card for every object (actresses) */}
-          {
-            actors.map(actor => (
-              <div key={actor.id} className="card">
+            allActors.map((actor, index) => (
+              <div key={index} className="card">
                 <div className="card_image">
                   <img src={actor.image} alt={actor.name} />
                 </div>
@@ -100,6 +81,11 @@ function App() {
         </div>
 
       </div>
+
+
+
+
+
     </>
   )
 }
